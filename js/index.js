b821dbbc5f12108a9a430c5546d73b07873fe9e0
RudychikFedorV <rudychik.fedor@gmail.com>
2024-04-09 18:49:09 +0300

    welcome swiper

--- js/index.js
+++ js/index.js
@@ -1,4 +1,36 @@
 function navToggle() {
-    document.getElementById("headerUl").classList.toggle("_active");
-    document.getElementById("headerBackdrop").classList.toggle("_active");
+  document.getElementById("headerUl").classList.toggle("_active");
+  document.getElementById("headerBackdrop").classList.toggle("_active");
 }
+window.onload = function () {
+  const swiper = new Swiper(".swiper", {
+    // direction: 'vertical',
+    loop: true,
+    slidesPerView: 1,
+    parallax: true,
+    speed: 1000,
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: ".welcome-left__number",
+      type: 'fraction',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: "#arrow-right",
+      prevEl: "#arrow-left",
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    breakpoints: {
+      300: {
+        slidesPerView: "auto",
+      },
+      992: {
+        slidesPerView: "auto",
+      },
+    },
+  });
+};
